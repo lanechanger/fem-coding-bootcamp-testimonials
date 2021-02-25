a0bba3b22c181affcdeaa2b2f451455a9d8b9f28
lanechanger <jeremy_ng_86@live.ca>
2021-02-24 23:50:40 -0500

    feat: created variables for buttons and the carousel elements being changed in JS

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+// console.log("hello world");
+const prev = document.querySelector("#previous-slide");
+const next = document.querySelector("#next-slide");
+
+const img = document.querySelector(".carousel-img");
+const testimonial = document.querySelector(".carousel__testimonial");
+const fullname = document.querySelector(".carousel__name");
+const role = document.querySelector(".carousel__role");
+
+prev.addEventListener("click", function(){
+  console.log("previous slide");
+  testimonial.classList.add("red");
+  fullname.classList.add("red");
+  role.classList.add("red");
+});
