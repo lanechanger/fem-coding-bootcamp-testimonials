6001dc39f3aecdede98cbfdf5d733733e754bd8f
lanechanger <jeremy_ng_86@live.ca>
2021-03-13 23:52:28 -0500

    fix: redid scripting to be simpler

--- app/js/script.js
+++ app/js/script.js
@@ -1,98 +1,39 @@
-// see site below for the tutorial/code that this is mostly based from
-// https://medium.com/@marcusmichaels/how-to-build-a-carousel-from-scratch-in-vanilla-js-9a096d3b98c9
-// I modified it to be more scalable but I have decided to not use an animation for the time being
-!(function (d) {
-  var itemClassNames = ["carousel-img", "carousel__testimonial", "carousel__person"],
-    slides = itemClassNames.map(function (e) {
-      return d.querySelectorAll("." + e);
-    }),
-    items = slides[0],
-    totalItems = items.length,
-    last = totalItems - 1,
-    slide = 0,
-    moving = true;
+// based on Apple Pie Giraffe's carousel article
+// https://applepiegiraffe.hashnode.dev/how-to-create-a-super-simple-image-slider
+const images = document.querySelectorAll('.carousel-img');
+const testimonials = document.querySelectorAll('.carousel__testimonial');
+const persons = document.querySelectorAll('.carousel__person');
+const prevBtn = document.querySelector('#previous-slide');
+const nextBtn = document.querySelector('#next-slide');
 
-  const prev = d.querySelector("#previous-slide"),
-    next = d.querySelector("#next-slide");
+let index = 0;
+images[index].classList.add('active');
+testimonials[index].classList.add('active');
+persons[index].classList.add('active');
 
-  // sets initial classes
-  function setInitialClasses() {
-    // for each of the nodeLists, set the corresponding classes
-    slides.forEach(function (e) {
-      e[last].classList.add("prev");
-      e[0].classList.add("active");
-      e[1].classList.add("next");
-    });
-  }
+function removeActive() {
+  images[index].classList.remove('active');
+  testimonials[index].classList.remove('active');
+  persons[index].classList.remove('active');
+}
 
-  // Set event listeners
-  function setEventListeners() {
-    next.addEventListener('click', moveNext);
-    prev.addEventListener('click', movePrev);
-  }
+function addActive() {
+  images[index].classList.add('active');
+  testimonials[index].classList.add('active');
+  persons[index].classList.add('active');
+}
 
-  // Next navigation handler
-  function moveNext() {
-    if (!moving) {
-      // wrap around if at the end, otherwise increment
-      if (slide === last) {
-        slide = 0;
-      } else {
-        slide++;
-      }
+function prevSlide() {
+  removeActive();
+  index = index === 0 ? images.length - 1 : index - 1;
+  addActive();
+}
 
-      moveCarouselTo(slide);
-    }
-  }
+function nextSlide() {
+  removeActive();
+  index = index === images.length - 1 ? 0 : index + 1;
+  addActive();
+}
 
-  // Previous navigation handler
-  function movePrev() {
-    if (!moving) {
-      // wrap around if at the beginning, otherwise decrement
-      if (slide === 0) {
-        slide = last;
-      } else {
-        slide--;
-      }
-
-      moveCarouselTo(slide);
-    }
-  }
-
-  // block out interactions for 500ms 
-  function disableInteraction() {
-    moving = true;
-    setTimeout(function () {
-      moving = false;
-    }, 500);
-  }
-
-  function moveCarouselTo(slide) {
-    if (!moving) {
-      disableInteraction();
-
-      // set new positions for the prev and next classes
-      const newPrev = (slide === 0) ? last : slide - 1,
-        newNext = (slide === last) ? 0 : slide + 1;
-
-      if (totalItems >= 3) {
-        // for each of carousel slide elements...
-        for (var x = 0; x < slides.length; x++) {
-          // set new flags
-          slides[x][newPrev].className = itemClassNames[x] + " carousel-slide prev";
-          slides[x][slide].className = itemClassNames[x] + " carousel-slide active";
-          slides[x][newNext].className = itemClassNames[x] + " carousel-slide next";
-        }
-      }
-    }
-  }
-
-  function initCarousel() {
-    setInitialClasses();
-    setEventListeners();
-
-    moving = false;
-  }
-
-  initCarousel();
-}(document));
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
